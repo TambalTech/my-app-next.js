af6b3c145e3c284300d8ecdaefb550aba07709a1
TambalTech <tambaldeveloper23@gmail.com>
2024-11-16 13:45:49 +0200

    create About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Welcome to SARU, your ultimate destination for everything movies! We are passionate movie enthusiasts who strive to bring the magic of cinema right to your fingertips. From the latest blockbusters to timeless classics, our mission is to keep you informed, entertained, and inspired by the world of film. Whether you’re a casual viewer or a dedicated cinephile, our platform is tailored to suit your love for storytelling on the big screen.</p>
+      <p>At SARU, we go beyond simple movie listings. We provide in-depth reviews, engaging articles, and insightful recommendations to help you navigate the ever-growing universe of cinema. Explore our curated collections, discover hidden gems, or dive into the world of your favorite genres with our personalized suggestions. Our commitment is to ensure every visitor finds something that resonates with their movie-loving heart.</p>
+      <p>We believe movies are more than entertainment—they’re a bridge to cultures, ideas, and emotions. That’s why we’re dedicated to fostering a vibrant community of film enthusiasts who share their passion, opinions, and experiences. Join us as we celebrate the art of filmmaking and the joy it brings to millions around the globe. Together, let’s make every movie moment unforgettable!</p>
+    </div>
+  )
+}
