9dcd64392ec5bc489c2d7cd0cb4eb6874bd71154
TambalTech <tambaldeveloper23@gmail.com>
2024-11-16 14:31:10 +0200

    create Navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import "./globals.css";
 import Header from "./component/Header";
 import Providers from "./Providers";
+import Navbar from './component/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       >
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
