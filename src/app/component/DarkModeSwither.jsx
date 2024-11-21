d1066f49bf345d848ae252a3af7961c7ca4ab5e3
TambalTech <tambaldeveloper23@gmail.com>
2024-11-21 14:19:23 +0200

    Add Loading Effect

--- src/app/component/DarkModeSwither.jsx
+++ src/app/component/DarkModeSwither.jsx
@@ -2,26 +2,35 @@
 import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-export default function DarkModeSwitch() {
+
+export default function DarkModeSwitcher() {
   const { theme, setTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
+  // Ensure component renders only after the client has mounted
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Avoid hydration error by ensuring client-side code is only run on the client
+  if (!mounted) return null; // Do not render anything until mounted
+
+  // Use the correct theme based on user/system preference
   const currentTheme = theme === 'system' ? systemTheme : theme;
-  useEffect(() => setMounted(true), []);
+
   return (
     <div>
-      {mounted &&
-        (currentTheme === 'dark' ? (
-          <MdLightMode
-            onClick={() => setTheme('light')}
-            className='text-xl cursor-pointer hover:text-amber-500'
-          />
-        ) : (
-          <MdDarkMode
-            onClick={() => setTheme('dark')}
-            className='text-xl cursor-pointer hover:text-amber-500'
-          />
-        ))}
+      {currentTheme === 'dark' ? (
+        <MdLightMode
+          onClick={() => setTheme('light')}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      ) : (
+        <MdDarkMode
+          onClick={() => setTheme('dark')}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      )}
     </div>
   );
 }
-
